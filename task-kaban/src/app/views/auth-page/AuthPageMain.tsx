4d4357250831871e3839c626be759b4a57a444f3
Dreno2003 <drenoaka2003@gmail.com>
2024-02-29 16:14:24 +0100

    finished auth page design

--- task-kaban/src/app/views/auth-page/AuthPageMain.tsx
+++ task-kaban/src/app/views/auth-page/AuthPageMain.tsx
@@ -1,28 +1,36 @@
-import React from 'react'
 import { ThemeSwitcher } from '@/components/theme-switcher/ThemeSwitcherMain'
 import { signInUser } from '@/services/auth/firebase'
+import logo from '@/assets/logo/taskjar-light.svg'
+import { Button } from '@/components/button/button';
+import { FcGoogle } from "react-icons/fc";
+
 function AuthPageMain() {
     async function handleUserAuth() {
         const authUser = await signInUser();
-        
+
     }
 
 
     return (
-        <div className='flex justify-center items-center h-screen text-center' >
+        <div className='flex bg-background justify-center items-center h-screen text-center' >
             <div>
-                <h3 className='text-2xl'>
+                <img src={logo} alt="task-jar log" className='mx-auto mb-4' width={50} />
+                <h3 className='text-2xl mb-2'>
+                    <b>
+                        Task jar
+                        {/* <ThemeSwitcher /> */}
 
-                    Task jar <ThemeSwitcher />
+                    </b>
                 </h3>
 
                 <p className='text-sm text-muted-foreground'>
                     Organise your task with ease
                 </p>
 
-                <button onClick={handleUserAuth}>
-                    CLICK TO AUTH
-                </button>
+                <Button size='lg' variant='outline' className=' mt-4 font-medium' onClick={handleUserAuth}>
+                    <FcGoogle className='mr-4 text-xl' />
+                    Sign In With Gooogle
+                </Button>
             </div>
 
         </div>
